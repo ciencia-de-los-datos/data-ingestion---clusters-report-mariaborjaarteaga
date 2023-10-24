d1a796d6895cfab2daf15b37f55b7bfe7d6412fa
María José Borja Arteaga <101903233+mariaborjaarteaga@users.noreply.github.com>
2023-10-24 14:05:30 -0500

    Actualizacion 5

--- pregunta.py
+++ pregunta.py
@@ -21,57 +21,33 @@ def ingest_data():
     #
     
     # Inicializa una lista vacía para almacenar las líneas a partir del patrón
-    lineas_desde_patron = []
-
-    # Abre el archivo 'nombre_del_archivo.txt' en modo lectura
-    with open('clusters_report.txt', 'r') as archivo:
-      recopilando = False
-      # Inicializa una cadena para acumular las líneas
-      acumulador = ""
-    
-      # Lee cada línea del archivo
-      for linea in archivo:
-        if recopilando:
-          if '.' in linea:
-            acumulador += linea.strip()  # Añade la línea sin el salto de línea
-            lineas_desde_patron.append(acumulador)
-            acumulador = ""  # Restablece el acumulador para la siguiente sección
-          else:
-            acumulador += linea.strip() + ' '  # Añade la línea sin el salto de línea
-        elif '----------' in linea:  # Encuentra el patrón
-          recopilando = True
-    data=[]
-    for l in lineas_desde_patron:
-      data.append(re.split(r'\s{2,}', l,3))
-    
-    # Crear el DataFrame con 4 columnas
-    df = pd.DataFrame(data, columns=['Columna1', 'Columna2', 'Columna3', 'Columna4'])
-    
-    cadena=df['Columna4'][5]
-    p= cadena.find(" 7")
-    te= cadena[:p]
-    df.loc[5,'Columna4']=te
-    
-    nr=cadena[p:]
-    
-    data2 = {nombre: valor for nombre, valor in zip(['Columna1', 'Columna2', 'Columna3', 'Columna4'], re.split(r'\s{2,}', nr,3))}
-    
-    
-    df = pd.concat([df.iloc[:6], pd.DataFrame([data2]), df.iloc[6:]]).reset_index(drop=True)
-    
-    
-    
-    
-    df['Columna4'] = df['Columna4'].str.replace(r'\s+', ' ', regex=True)
-    
-    df['Columna4'] = df['Columna4'].str.replace(r', ', ',', regex=True)
-        
-    df['Columna4'] = df['Columna4'].str.replace(',', ', ', regex=True)
-    df['Columna3'] = df['Columna3'].str.replace(r' %', '', regex=True)
-    df['Columna3'] = df['Columna3'].str.replace(r',', '.', regex=True)
-    df['Columna4'] = df['Columna4'].str.replace(r'.', '', regex=True)
+    archivo = open("clusters_report.txt", mode='r')
     nombres = ["cluster", "cantidad de palabras clave", "porcentaje de palabras clave", "principales palabras clave"]
-    #df["cantidad de palabras clave"] = df["cantidad de palabras clave"].astype(int)
+    archivo.readline()
+    archivo.readline()
+    archivo.readline()
+    archivo.readline()
+    data= archivo.read()
+    archivo.close()
+    data = ' '.join(''.join(data).split())
+    data = data.split('.')
+    data.pop()
+    aux1 = []
+    aux2 = []
+    
+    aux1 = re.split('([o][l][ ])+',data[5])[0] + re.split('([o][l][ ])+',data[5])[1]
+    aux2 = re.split('([o][l][ ])+',data[5])[2]
+    
+    data[5] = aux1
+    data.insert(6, aux2)
+    txt = []
+    
+    data.pop()
+    
+    for i in data:
+      txt.append(i.split('%')[0].replace(',','.').split()+[i.split('%')[1].strip()])
+        
+    df = pd.DataFrame(txt, columns=['Columna1', 'Columna2', 'Columna3', 'Columna4'])
     df.columns = nombres
     df.columns = df.columns.str.replace(' ', '_')
     
